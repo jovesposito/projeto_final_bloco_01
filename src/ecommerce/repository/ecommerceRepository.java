d888d8ead4374f2577b4bd11cbc29489a622f616
jovesposito <jovesposito@outlook.com>
2024-06-20 16:01:02 -0300

    Ajustes nos métodos de busca e atualização

--- src/ecommerce/repository/ecommerceRepository.java
+++ src/ecommerce/repository/ecommerceRepository.java
@@ -5,13 +5,9 @@ import ecommerce.model.Produto;
 public interface ecommerceRepository {
 
 	public void cadastrarProduto(Produto produto);
-
 	public void listarProdutos();
-
 	public void buscarPorId(int id);
-
 	public void atualizarProduto(Produto produto);
-
 	public void deletarProduto(int id);
 
 }
